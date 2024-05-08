a4ef3ccf668f7644bbb31c315c3c30cc8c455e56
Sheezzz0 <junknewera@gmail.com>
2024-05-08 04:34:53 +0300

    Finished simple pomodoro

--- main.py
+++ main.py
@@ -2,21 +2,41 @@ import time
 
 
 class Pomodoro:
-    def __init__(self, focus_minutes):
-        self.focus_minutes = focus_minutes
-        self.break_minutes = focus_minutes / 6
+    def __init__(self, focus_minutes=60, focus_sessions=4):
+        self.focus_seconds = focus_minutes * 60
+        self.break_seconds = int(focus_minutes * 10)
+        self.focus_sessions = focus_sessions
         self.is_focus = True
         pass
 
-    def focus_start(self):
-        if self.is_focus:
-            pass
+    def start_timer(self):
+        for _ in range(self.focus_sessions):
+            if not self.is_focus:
+                self.timer(self.break_seconds, "Break time")
+                self.is_focus = True
+            self.timer(self.focus_seconds, "Focus time")
+            self.is_focus = False
+        print("Nice work!")
 
     @staticmethod
-    def timer(self, minutes):
-        min_to_sec = minutes * 60
-        for sec in range(min_to_sec, 0, -1):
-            pass
+    def timer(total_seconds, name):
+        for sec in range(total_seconds, 0, -1):
+            hours = int(sec / 3600)
+            minutes = int(sec / 60) % 60
+            seconds = sec % 60
+            print(f"{name}: {hours:02}:{minutes:02}:{seconds:02}")
+            time.sleep(0.05)
 
-pomodoro = Pomodoro(63)
-pomodoro.focus_start()
+
+def main():
+    try:
+        minutes = int(input("Input focus time in minutes: "))
+        sessions = int(input("Input total focus sessions: "))
+        pomodoro = Pomodoro(minutes, sessions)
+    except ValueError:
+        pomodoro = Pomodoro()
+    pomodoro.start_timer()
+
+
+if __name__ == "__main__":
+    main()
